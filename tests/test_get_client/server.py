c6fcb2a6cc0210fc869c0e2b065837b6e796a607
sheepy0125 <sheepy404@gmail.com>
2022-02-04 21:48:50 -0500

    Delete old broken test

--- tests/test_get_client/server.py
+++
@@ -1,9 +0,0 @@
-# Dummy server
-
-from hisock import start_server
-from hisock.utils import _input_port
-
-server = start_server(("127.0.0.1", _input_port("Port: ")))
-
-while True:
-    server.run()
