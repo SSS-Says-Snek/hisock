0779e0f35dda9cbb8eda8721f0b93dba6235a989
Sheepy <sheepy404@gmail.com>
2022-03-18 04:14:05 +0000

    Add a newline between line 120-121

--- hisock/client.py
+++ hisock/client.py
@@ -118,6 +118,7 @@ class HiSockClient(_HiSockBase):
     :ivar int connect_time: An integer sotring the Unix timestamp of when the
         client connected to the server.
     """
+    
     def __init__(
         self,
         addr: tuple[str, int],
